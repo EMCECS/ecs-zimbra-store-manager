b8fb0259523d49f2410231e3de3259d1e1cc5593
seibed <david.seibert@emc.com>
2016-10-07 13:00:04 -0400

    Adding primary license and file copyrights.

--- src/main/java/com/emc/ecs/zimbra/integration/S3ClientFactory.java
+++ src/main/java/com/emc/ecs/zimbra/integration/S3ClientFactory.java
@@ -1,17 +1,36 @@
+/*
+ * Copyright (c) 2016 EMC Corporation. All Rights Reserved.
+ *
+ * Licensed under the EMC Software License Agreement for Free Software (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * https://github.com/EMCECS/ecs-zimbra-store-manager/blob/master/LICENSE
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package com.emc.ecs.zimbra.integration;
 
 import com.emc.ecs.zimbra.ext.config.Configuration;
 import com.emc.ecs.zimbra.ext.config.ConfigurationFactory;
 import com.emc.ecs.zimbra.ext.config.ConfigurationFactoryUtil;
+import com.emc.ecs.zimbra.ext.config.PropertiesConfigurationDecorator;
 //import com.emc.ecs.zimbra.integration.util.EcsLogger;
 import com.emc.object.Protocol;
 import com.emc.object.s3.S3Config;
 import com.emc.object.s3.jersey.S3JerseyClient;
+import com.emc.object.util.ConfigUri;
 import com.emc.rest.smart.ecs.Vdc;
+import com.zimbra.common.util.StringUtil;
 
 //import static com.amazonaws.SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -32,15 +51,65 @@ public class S3ClientFactory {
 //        }
 
         S3Config s3Config;
-        if (configuration.useSmartClient() == true) {
-            s3Config = new S3Config(getClientProtocol(), getVdcs(configuration.getEndpoints()));
+        if (configuration.getS3ConfigUri() != null) {
+            ConfigUri<S3Config> s3Uri = new ConfigUri<S3Config>(S3Config.class);
+            s3Config = s3Uri.parseUri(configuration.getS3ConfigUri());
         } else {
-            s3Config = new S3Config(new URI(getFirstEndpoint(configuration.getEndpoints())));
+            checkProperties(configuration);
+            if (configuration.useSmartClient() == true) {
+                s3Config = new S3Config(getClientProtocol(), getVdcs(configuration.getEndpoints()));
+            } else {
+                s3Config = new S3Config(new URI(getFirstEndpoint(configuration.getEndpoints())));
+            }
+            s3Config.setIdentity(configuration.getAccessKey());
+            s3Config.setSecretKey(configuration.getSecretKey());
         }
-        s3Config.setIdentity(configuration.getAccessKey());
-        s3Config.setSecretKey(configuration.getSecretKey());
 
         return new S3JerseyClient(s3Config);
+    }
+
+    /**
+     * @param configuration
+     */
+    private static void checkProperties(Configuration configuration) {
+        List<String> missingProperties = new ArrayList<String>(5);
+        if (configuration.useSmartClient() == null) {
+            missingProperties.add(PropertiesConfigurationDecorator.ECS_USE_SMART_CLIENT);
+        }
+        if (isNullOrEmpty(configuration.getAccessKey())) {
+            missingProperties.add(PropertiesConfigurationDecorator.ECS_ACCESS_KEY);
+        }
+        if (isNullOrEmpty(configuration.getClientProtocol())) {
+            missingProperties.add(PropertiesConfigurationDecorator.ECS_CLIENT_PROTOCOL);
+        }
+        if (isNullOrEmpty(configuration.getEndpoints())) {
+            missingProperties.add(PropertiesConfigurationDecorator.ECS_ENDPOINTS);
+        }
+        if (isNullOrEmpty(configuration.getSecretKey())) {
+            missingProperties.add(PropertiesConfigurationDecorator.ECS_SECRET_KEY);
+        }
+        if (missingProperties.size() > 0) {
+            String errorMessage = "You must set a value for either the property " + PropertiesConfigurationDecorator.ECS_S3_CONFIG_URI + ", or for ";
+            for (int i = 0; i < missingProperties.size(); ++i) {
+                errorMessage = errorMessage + missingProperties.get(i);
+                if (i < missingProperties.size() - 2) {
+                    errorMessage = errorMessage + ", ";
+                } else if (i == missingProperties.size() - 2) {
+                    errorMessage = errorMessage + " and ";
+                } else if (i == missingProperties.size() - 1) {
+                    errorMessage = errorMessage + ".";
+                }
+            }
+            throw new RuntimeException(errorMessage);
+        }
+    }
+
+    /**
+     * @param string
+     * @return
+     */
+    private static boolean isNullOrEmpty(String string) {
+        return StringUtil.isNullOrEmpty(string);
     }
 
     /**
--- src/main/java/com/emc/ecs/zimbra/integration/util/EcsProgressListener.java
+++ src/main/java/com/emc/ecs/zimbra/integration/util/EcsProgressListener.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2016 EMC Corporation. All Rights Reserved.
+ *
+ * Licensed under the EMC Software License Agreement for Free Software (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * https://github.com/EMCECS/ecs-zimbra-store-manager/blob/master/LICENSE
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package com.emc.ecs.zimbra.integration.util;
 
 import com.emc.object.util.ProgressListener;
