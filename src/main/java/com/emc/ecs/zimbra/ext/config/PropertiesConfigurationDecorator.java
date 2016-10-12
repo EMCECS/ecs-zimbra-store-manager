bb3a856ee7facdd1cf17999058a693cc462db551
seibed <david.seibert@emc.com>
2016-10-12 15:37:18 -0400

    Updating copyrights, minor doc tweak.

--- src/main/java/com/emc/ecs/zimbra/ext/config/PropertiesConfigurationDecorator.java
+++ src/main/java/com/emc/ecs/zimbra/ext/config/PropertiesConfigurationDecorator.java
@@ -7,10 +7,7 @@
  *
  * https://github.com/EMCECS/ecs-zimbra-store-manager/blob/master/LICENSE.txt
  *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * or in the "LICENSE.txt" file accompanying this file.
  */
 package com.emc.ecs.zimbra.ext.config;
 
